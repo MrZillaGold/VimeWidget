11419ef0a77246e619e67df174c94b77b0365a30
Egor <mrzillagold@gmail.com>
2020-04-16 10:30:49 +1000

    Немного красоты

--- index.mjs
+++ index.mjs
@@ -1,19 +1,20 @@
 import { Widget } from "./modules/widget";
 
-const widget = new Widget();
-
-widget.UpdateWidget();
-
-setInterval(() => {
+const updateWidget = () => {
     const widget = new Widget();
 
     widget.UpdateWidget();
-}, 15 * 60 * 1000); // Обновление виджета происходит раз в 15 минут, так как информация о гильдии обновляется раз в 10-15 минут.
+};
+
+updateWidget();
+
+setInterval(updateWidget, 15 * 60 * 1000); // Обновление виджета происходит раз в 15 минут, так как информация о гильдии обновляется раз в 10-15 минут.
 
 console.log("[VimeWidget] Запущен!");
 
 //
 // Made with ♥ by MrZillaGold (https://vk.com/mrzillagold)
 //
-// VimeWidget доступен по лицензии Creative Commons «Attribution-NonCommercial-ShareAlike» («Атрибуция — Некоммерческое использование — На тех же условиях») 4.0 Всемирная.
+// VimeWidget доступен по лицензии Creative Commons «Attribution-NonCommercial-ShareAlike»
+// («Атрибуция — Некоммерческое использование — На тех же условиях») 4.0 Всемирная.
 //
